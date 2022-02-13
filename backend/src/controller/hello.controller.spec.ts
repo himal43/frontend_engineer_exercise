ba021b306c8d010769caef3d772674bccead741d
Himal <himal.dhamala@mail.com>
2022-02-13 14:42:48 -0500

    new commit

--- backend/src/controller/hello.controller.spec.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import { createApp } from "../app";
-
-describe("HelloController", () => {
-  it("returns 200 on GET /", (done) => {
-    request(createApp()).get("/").expect(200, done);
-  });
-});
